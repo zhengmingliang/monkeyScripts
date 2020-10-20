dffe0a35e5cc93eb5fead0889e3f2d257dd4536d
zhengmingliang <1072307340@qq.com>
2020-10-20 20:50:07 +0800

    新增阅读全文脚本，支持CSDN和github.io部分个人站的博文不用关注即可阅读全文

---
+++ readAll/readAll.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         阅读全文
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io
+// @author       zhengmingliang
+// @match        https://blog.csdn.net/*
+// @match        *://*.github.io/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    var $ = $ || window.$;
+    var href = window.location.href
+    // csdn
+    if (href.indexOf('csdn') != -1) {
+        console.log("检测到CSDN。。。。")
+        if ($("#btn-readmore-zk").length > 0) {
+            console.log("检测到有阅读全文关注限制。。。。")
+            // 移除阅读全文
+            $("#btn-readmore-zk").parent().remove();
+            // 使滚动条可见
+            // $("#article_content").css('overflow','auto')
+            // 优化后：直接将style置为空
+            $("#article_content").prop('style', '')
+            console.log("已解除阅读全文关注限制。。。。")
+        }
+    } else if (href.indexOf('github.io') != -1) {
+        console.log("检测到github.io。。。。")
+        if ($("#read-more-btn").length > 0) {
+            console.log("检测到有阅读全文关注限制。。。。")
+            // 移除阅读全文
+            $("#read-more-btn").parent().remove();
+            // 使滚动条可见
+            // $("#container").css('overflow','auto')
+            // 优化后：直接将style置为空
+            $("#container").prop('style', '')
+            console.log("已解除阅读全文关注限制。。。。")
+
+        }
+        // Your code here...
+    }
+})();

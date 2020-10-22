bac2ecaf9d514f370a80e06c2d7ae43be04ecce6
zhengmingliang <1072307340@qq.com>
2020-10-22 21:17:13 +0800

    添加xz577.com网站的支持

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         阅读全文
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io
+// @version      0.4
+// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com
 // @author       zhengmingliang
 // @match        https://blog.csdn.net/*
 // @match        *://*.github.io/*
+// @match        *://*.xz577.com/*
 // @grant        none
 // ==/UserScript==
 
@@ -42,6 +43,19 @@
             // $("#container").css('overflow','auto')
             // 优化后：直接将style置为空
             $("#container").prop('style', '')
+            console.log("已解除阅读全文关注限制。。。。")
+
+        }
+    }else if (href.indexOf('xz577') != -1) { //www.xz577.com
+        console.log("检测到xz577。。。。")
+        if ($(".m-zk").length > 0) {
+            console.log("检测到有阅读全文关注限制。。。。")
+            // 移除阅读全文
+            $(".m-zk").parent().remove();
+            // 使滚动条可见
+            // $("#container").css('overflow','auto')
+            // 优化后：直接将style置为空
+            $("#mewsmian").prop('style', '')
             console.log("已解除阅读全文关注限制。。。。")
 
         }

c8ea4c1efae9f794326742ef2badd837fa50ea59
zhengmingliang <1072307340@qq.com>
2021-05-05 16:48:39 +0800

    处理csdn 非登录状态下经常弹二维码登录框问题

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         阅读全文、自动展开全文、自动移除万恶弹框
 // @namespace    http://tampermonkey.net/
-// @version      2.10.0
+// @version      2.10.1
 // @require      https://greasyfork.org/scripts/415668-zmquery3-5-1/code/zmQuery351.js?version=866815
 // @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）
 // @author       zhengmingliang
@@ -380,6 +380,7 @@
         readAllRule1(".btn-readmore", "#article_content")
         // 移动端处理
         readAllRule1(".btn_mod", ".article_content")
+        $$$("style").get(0).append(".login-box{display:none !important} \r\n .login-mark{display:none !important}");
 
     } else if (href.indexOf('github.io') != -1) { //hoxis.github.io
         console.log("检测到github.io。。。。")

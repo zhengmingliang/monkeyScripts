9858fa1a818eef3a808b32f0d035e11c7cec4dd9
zhengmingliang <1072307340@qq.com>
2021-05-05 17:40:09 +0800

    添加 百家号、百度文章 支持

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         阅读全文、自动展开全文、自动移除万恶弹框
 // @namespace    http://tampermonkey.net/
-// @version      2.10.2
+// @version      2.11.0
 // @require      https://greasyfork.org/scripts/415668-zmquery3-5-1/code/zmQuery351.js?version=866815
-// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）
+// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章
 // @author       zhengmingliang
 // @match        https://*.csdn.net/*
 // @match        *://*.github.io/*
 // @match        *://*.xz577.com/*
 // @match        *://*.javascriptcn.com/*
+// @match        *://*.baidu.com/*
 // @match        *://*.iteye.com/*
 // @match        *://*.720ui.com/*
 // @match        *://www.wukong.com/question/*
@@ -227,6 +228,10 @@
      */
    function addLayerCssStyle() {
        $$$("style").get(0).append(".layui-layer-shade{display:none !important} .layui-layer-page{display:none !important}")
+
+    }
+    function addLayerCssStyle2() {
+       $$$("style").get(0).append(".layer-main{display:none !important}")
 
     }
 
@@ -453,6 +458,18 @@
             $$$(".article_comment").css("display","block")
         }, 1000)
 
+    }  else if (href.indexOf('baijiahao') != -1|| href.indexOf('mbd.baidu.com') != -1) { // 百度文章
+        console.log("检测到百家号。。。。")
+        // 循环检测
+       let interval = setInterval(function () {
+            console.log("轮训检测...")
+            if ($$$(".oPadding").length > 0) {
+                readAllRule1(".oPadding", ".mainContent",true)
+                $$$(".scrollHide").removeClass("scrollHide");
+                clearInterval(interval)
+            }
+        }, 1000)
+        addLayerCssStyle2();
     } else if (href.indexOf('www.elecfans.com') != -1) { // elecfans.com
         console.log("检测到电子发烧友。。。。")
         // 循环检测

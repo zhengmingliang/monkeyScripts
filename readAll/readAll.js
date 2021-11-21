6bd5f08b9d34714caf26db99c36b54c39d10e0f6
zhengmingliang <1072307340@qq.com>
2021-11-21 21:40:31 +0800

    添加简书移动版支持

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         阅读全文、自动展开全文、自动移除万恶弹框
 // @namespace    http://tampermonkey.net/
-// @version      2.11.2
+// @version      2.12.1
 // @require      https://greasyfork.org/scripts/415668-zmquery3-5-1/code/zmQuery351.js?version=866815
-// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章
+// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章、简书移动版
 // @author       zhengmingliang
 // @match        https://*.csdn.net/*
 // @match        *://*.github.io/*
+// @match        *://*.jianshu.com/p/*
 // @match        *://*.xz577.com/*
 // @match        *://*.javascriptcn.com/*
 // @match        *://*.baidu.com/*
@@ -364,6 +365,27 @@
         }
 
     }
+
+    /**
+     * 公共隐藏
+     * @param selectors
+     * @param isRemoveParent
+     */
+    function commonHideRule1(selectors) {
+        var selectorArray = selectors.split(",");
+        for (let index in selectorArray) {
+            if ($$$("style").length > 0) {
+                $$$("style").get(0).append(selectorArray[index] + "{display:none !important}");
+            }else {
+                $$$("header").append("<style></style>");
+                $$$("style").get(0).append(selectorArray[index] + "{display:none !important}");
+            }
+        }
+
+
+
+
+    }
     var $$$ = $ || window._$ || zmQuery;
     var href = window.location.href
 
@@ -393,6 +415,11 @@
         console.log("检测到github.io。。。。")
         readAllRule1("#read-more-btn", "#container")
         addDisplayCssStyle();
+    }  else if (href.indexOf('jianshu.com') != -1) { //jianshu.com
+        console.log("检测到jianshu.com。。。。")
+        // $$$("style").get(0).append(".download-app-guidance{display:none !important}.call-app-btn{display:none !important}");
+        commonHideRule1(".download-app-guidance,.call-app-btn,#jianshu-header")
+        readAllRule1(".close-collapse-btn", ".collapse-free-content",)
     }  else if (href.indexOf('jingyan.baidu.com') != -1) { //百度经验
         console.log("检测到百度经验。。。。")
         readAllRule2(".read-whole-mask", ".exp-content-container.fold","fold")

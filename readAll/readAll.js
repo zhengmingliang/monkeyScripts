4911f48f945b7ce016fdf169a20714b9e3269782
zhengmingliang <1072307340@qq.com>
2021-11-27 17:40:58 +0800

    goodreads（PC、移动版）

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         阅读全文、自动展开全文、自动移除万恶弹框
 // @namespace    http://tampermonkey.net/
-// @version      2.13.0
+// @version      2.14.0
 // @require      https://greasyfork.org/scripts/415668-zmquery3-5-1/code/zmQuery351.js?version=866815
-// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章、简书移动版、搜狐移动版
+// @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章、简书移动版、搜狐移动版、goodreads（PC、移动版）
 // @author       zhengmingliang
 // @match        https://*.csdn.net/*
 // @match        *://*.github.io/*
@@ -16,6 +16,7 @@
 // @exclude      https://tieba.baidu.com/*
 // @match        *://*.iteye.com/*
 // @match        *://*.720ui.com/*
+// @match        *://*.goodreads.com/book/show/*
 // @match        *://www.wukong.com/question/*
 // @match        *://cloud.tencent.com/*
 // @match        *://m.leju.com/*
@@ -629,6 +630,23 @@
             if($$$("._1mAOD6Nkgp2wM7xlGCHeNi").length > 0){
                 commonRemoveRules1(["._1mAOD6Nkgp2wM7xlGCHeNi","._1GTaS1LTuTrKnZ-oQ6KFRG"],true)
                 commonReadAllRule1()
+                clearInterval(interval)
+            }
+
+        },1000)
+
+    } else if (href.indexOf('goodreads.com') != -1) { // goodreads.com
+        console.log("检测到goodreads.com。。。。")
+        // 循环检测
+        let interval = setInterval(function (){
+            console.log("轮训检测...")
+
+            if($$$("a[onclick^=swapContent").length > 0){
+                // PC 版本
+                $$$(".readable >  span:nth-child(2)").attr("style","display:blocked")
+                // 移动版本
+                $$$(".bookReviewBody >  span:nth-child(2)").attr("style","display:blocked")
+                $$$("a[onclick^=swapContent").attr("style","display:none")
                 clearInterval(interval)
             }
 

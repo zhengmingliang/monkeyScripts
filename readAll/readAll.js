dd377410734cdd052c245dd8ab886f35920577a5
zhengmingliang <1072307340@qq.com>
2021-11-28 14:28:43 +0800

    修复百度贴吧图片无法展示问题

--- readAll/readAll.js
+++ readAll/readAll.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         阅读全文、自动展开全文、自动移除万恶弹框
 // @namespace    http://tampermonkey.net/
-// @version      2.14.0
+// @version      2.14.1
 // @require      https://greasyfork.org/scripts/415668-zmquery3-5-1/code/zmQuery351.js?version=866815
 // @description  【非自动关注】【自用，长期维护】【功能有】1. 阅读全文网站支持：CSDN、github.io、xz577.com、iteye.com、720ui.com、cloud.tencent.com、新浪、头条、网易新闻、腾讯新闻、51CTO、知乎、果壳科技（移动版）、awesomes.cn、javascriptcn.com、人民日报（移动版）、凤凰网、虎扑移动版、百度经验、360文档（个人图书馆）、乐居买房（移动版）、电子发烧友网（PC、移动版）、悟空问答（PC、移动版）、百家号、百度文章、简书移动版、搜狐移动版、goodreads（PC、移动版）
 // @author       zhengmingliang
@@ -13,7 +13,7 @@
 // @match        *://*.javascriptcn.com/*
 // @match        *://*.baidu.com/*
 // @exclude      https://www.baidu.com/*
-// @exclude      https://tieba.baidu.com/*
+// @exclude      *://tieba.baidu.com/*
 // @match        *://*.iteye.com/*
 // @match        *://*.720ui.com/*
 // @match        *://*.goodreads.com/book/show/*
